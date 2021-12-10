0bd3c1d7d83c4cae5331a42027400a1f0b783e27
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-12-10 08:54:16 +0100

    RellenarMatriz y Utilidades

--- RellenaMatriz.java
+++ RellenaMatriz.java
@@ -1,5 +1,22 @@
 public class RellenaMatriz {
+
     public static void main(String[] args) {
-        
+        int [][] miMatriz = new int [10][10];
+        Utilidades.rellenaMatriz(miMatriz,0,1);
+        mMatriz(miMatriz);
     }
+    
+    public static void mMatriz(int matriz[][]) {
+        int i,j; 
+        int filas = matriz.length;
+        int columnas = matriz[0].length;
+        // Recorrido de las filas de la matriz
+        for (i=0; i < filas; i++) {
+                // Recorrido de las celdas de una fila
+                for (j=0; j<columnas; j++) {
+                    System.out.println("matriz[" + i + "][" + j + "]=" + matriz[i][j] );
+            }
+        }
+    }
+    
 }
