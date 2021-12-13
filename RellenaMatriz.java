5460e55f7e46e8606d1ac301e545c3c1dd6a6610
CandelaLucena <candelalucenareyes1212@gmail.com>
2021-12-13 08:22:52 +0100

    Rellenar Matriz terminado

--- RellenaMatriz.java
+++ RellenaMatriz.java
@@ -14,7 +14,7 @@ public class RellenaMatriz {
         for (i=0; i < filas; i++) {
                 
                 for (j=0; j<columnas; j++) {
-                    System.out.println(matriz[i][j]+" ");
+                    System.out.print(matriz[i][j]+" ");
             }
             System.out.println();
         }
